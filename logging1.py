f0f7285e4215c5f7cec3a0384ab009c162138d58
Orkhan <68821672+OrxanElibeyli@users.noreply.github.com>
2021-09-26 13:36:43 +0400

    Update logging1.py

--- logging1.py
+++ logging1.py
@@ -4,10 +4,7 @@ class Logging1():
     LOG_FILE_PATH='C:\\Users\\ORXAN\\Documents\\log.txt'
 
     
-
     def log(self,severity, message):
-        file = open(self.LOG_FILE_PATH,'a')
-        line = str(datetime.now()) + ':  ' + severity + ':  ' + message + '\n'
-        #print(line)
-        file.write(line)
-        file.close()
+        with open(self.LOG_FILE_PATH, 'a', encoding='utf8') as file:
+            line = str(datetime.now()) + ':  ' + severity + ':  ' + message + '\n'
+            file.write(line)
