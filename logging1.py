8b029505e5c655502e255d2d3541ce16339f42c3
Orkhan <68821672+OrxanElibeyli@users.noreply.github.com>
2021-10-03 18:58:18 +0400

    alpha version

--- logging1.py
+++ logging1.py
@@ -1,10 +1,15 @@
 from datetime import datetime
+import sys
 
 class Logging1():
     LOG_FILE_PATH='C:\\Users\\ORXAN\\Documents\\log.txt'
 
     
     def log(self,severity, message):
-        with open(self.LOG_FILE_PATH, 'a', encoding='utf8') as file:
-            line = str(datetime.now()) + ':  ' + severity + ':  ' + message + '\n'
-            file.write(line)
+        try:
+            with open(self.LOG_FILE_PATH, 'a', encoding='utf8') as file:
+                line = str(datetime.now()) + ':  ' + severity + ':  ' + message + '\n'
+                file.write(line)
+        except:
+            print('something went wrong while log file processing.')
+
