7759d6d468117510f67f82d9876ad4582dd65359
Orkhan <68821672+OrxanElibeyli@users.noreply.github.com>
2021-12-16 17:36:33 +0400

    Update cl.py

--- cl.py
+++ cl.py
@@ -1,9 +1,14 @@
+######################################################
+#  -----------------------------------------------   #
+# | dst addr | len of outgoing message | message |   #
+# ------------------------------------------------   #
+######################################################
+
 import socket
 import pdb
 
 
-print('*client was started...')
-print('*trying to connect server...')
+
 
 
 SERVER = '192.168.3.71'
@@ -13,7 +18,38 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((SERVER,PORT))
 
 print('*connected to the server')
-while(True):
-    message = input('enter something:')
 
-    sock.sendall(message.encode())
+
+#000.000.000.000  15
+#192.168.0.1
+
+def format_IP(str):
+    IP = str
+
+    if(len(str)<15):
+        while(len(str)<15):
+            str = str + '*'
+
+        IP = str
+
+    return IP
+
+def format_len(message):
+    leng = len(message)
+    if(leng<10):
+        leng = leng + '*'
+
+    return leng
+        
+
+
+dst = input("enter destination:")
+dst = format_IP(dst)
+
+while(True):
+
+    message = input('enter message:') 
+
+    sock.sendall(dst.encode('utf-8')) #send first header (destination)
+    sock.sendall(format_len(message).encode('utf-8'))  #send second header (len of outgoing message)
+    sock.sendall(message.encode('utf-8')) #send third header (message itself)
