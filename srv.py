f617ebf5a9e4b86369ebf8c5086a72d0e31df611
Orkhan <68821672+OrxanElibeyli@users.noreply.github.com>
2021-12-16 17:35:33 +0400

    Update srv.py

--- srv.py
+++ srv.py
@@ -1,3 +1,9 @@
+######################################################
+#  -----------------------------------------------   #
+# | dst addr | len of incoming message | message |   #
+# ------------------------------------------------   #
+######################################################
+
 import socket
 import threading
 
@@ -7,17 +13,33 @@ clients = []
 HOST = ''
 PORT = 4224
 
+LEN_OF_IP = 15
+
+
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
 sock.listen()
 
 
-
+#this function is used by threat for receiving messages from client
 def receive_mesages(conn):
     while(True):
-        message = conn.recv(10)
-        print(message)
+        #first header of TCP
+        dest_addr = conn.recv(LEN_OF_IP)
+
+        #second header of TCP
+        len_of_incoming_message = conn.recv(2)
+
+        #third header of TCP
+        message = conn.recv(len_of_incoming_message)
+
+        package = conn + " | " + dest_addr + " | " + message
+
+
+        message_file = open("messages.txt","w+")
+        message_file.write(package)
+        message_file.close()
     
 
 while(True):
