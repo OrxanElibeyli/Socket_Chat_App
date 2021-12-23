a2f56395eb24679a5b4273de2919808fd24c23d2
Orkhan <68821672+OrxanElibeyli@users.noreply.github.com>
2021-12-23 18:56:05 +0400

    Update srv.py

--- srv.py
+++ srv.py
@@ -1,19 +1,29 @@
+#receive header
 ######################################################
 #  -----------------------------------------------   #
-# | dst addr | len of incoming message | message |   #
+# | receiver | len of incoming message | message |   #
+# ------------------------------------------------   #
+######################################################
+
+#send header
+######################################################
+#  -----------------------------------------------   #
+# | sender | len of outgoing message | message   |   #
 # ------------------------------------------------   #
 ######################################################
 
 import socket
 import threading
+import re
 
 
 clients = []
 
 HOST = ''
 PORT = 4224
-
 LEN_OF_IP = 15
+
+established_connections = []
 
 
 
@@ -23,31 +33,83 @@ sock.listen()
 
 
 #this function is used by threat for receiving messages from client
-def receive_mesages(conn):
+def receive_messages(conn):
     while(True):
-        #first header of TCP
-        dest_addr = conn.recv(LEN_OF_IP)
-
-        #second header of TCP
-        len_of_incoming_message = conn.recv(2)
-
-        #third header of TCP
-        message = conn.recv(len_of_incoming_message)
-
-        package = conn + " | " + dest_addr + " | " + message
+        #first header of packet
+        receiver = conn.recv(LEN_OF_IP).decode('utf-8')
+        receiver = receiver.replace('*','')
 
 
-        message_file = open("messages.txt","w+")
+        #second header of packet
+        len_of_incoming_message = conn.recv(2).decode('utf-8')
+        len_of_incoming_message = len_of_incoming_message.replace('*','')
+
+        #third header of packet
+        message = conn.recv(int(len_of_incoming_message)).decode('utf-8')
+
+        sender = conn.getpeername()[0]
+
+        package = sender + "|" + receiver + "|" + message + "\n"
+
+        message_file = open("messages.txt","a")
         message_file.write(package)
         message_file.close()
     
 
+
+#192.168.6.15  ===> 192.168.6.15***
+def format_IP(str):
+    IP = str
+
+    if(len(str)<15):
+        while(len(str)<15):
+            str = str + '*'
+
+        IP = str
+
+    return IP
+
+
+def format_len(message):
+    leng = len(message)
+    if(leng<10):
+        return str(leng) + '*'
+
+    return str(leng)
+
+def send_messages():
+    message_file = open("messages.txt","r")
+    while(True):
+        if(established_connections):
+            for line in message_file:
+                headers = line.split('|', 3)
+                #print("!!!!!!!! ----> ", established_connections[0].getpeername()[0])
+                for established_connection in established_connections:
+                    #print("getpeername - ->", established_connection.getpeername[0])
+                    if(established_connection.getpeername()[0] == headers[1]):
+                        
+                        print('-------',format_IP(headers[0]))
+
+                        #IP address of sender (first header of packet)
+                        established_connection.sendall(format_IP(headers[0]).encode('utf-8'))   
+
+                        print('--------',format_len(headers[2]))
+                        #send len of message (second header of packet)
+                        established_connection.sendall(format_len(headers[2]).encode('utf-8'))
+
+                        print('--------',headers[2])
+                        #send message (third header of message)
+                        established_connection.sendall(headers[2].encode('utf-8'))        
+
+
+
+
+t_send = threading.Thread(target=send_messages)
+t_send.start()
+
 while(True):
     conn, addr = sock.accept()
-    print('connection ---> ', conn)
-    print('address --->',addr)
+    established_connections.append(conn)
 
-    t1 = threading.Thread(target=receive_mesages, args=(conn,))
+    t1 = threading.Thread(target=receive_messages, args=(conn,))
     t1.start()
-    
-
