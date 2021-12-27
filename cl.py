f6a55cdccaf9167545382c6dd35a174689135d32
Orkhan <68821672+OrxanElibeyli@users.noreply.github.com>
2021-12-27 16:47:46 +0400

    Update cl.py

--- cl.py
+++ cl.py
@@ -13,10 +13,8 @@
 ######################################################
 
 import socket
-import os
 import threading
-
-
+import ipaddress
 
 
 
@@ -24,10 +22,29 @@ SERVER = '192.168.3.71'
 PORT = 4224
 LEN_OF_IP = 15
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((SERVER,PORT))
 
-print('*connected to the server')
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    sock.connect((SERVER,PORT))
+    print('connected to the server')
+except Exception as e:
+    print('this exception occured: -- >', e)
+    print('something went wrong while connecting to the server. Probably server is not up')
+    print('exiting...')
+    quit()
+        
+
+
+def validate_ip_addresses(ip_address):
+    try:
+        ipaddress.ip_address(ip_address)
+        return True
+    except:
+        pass
+
+    return False
+
 
 
 #192.168.6.15  ===> 192.168.6.15***
@@ -51,21 +68,26 @@ def format_len(message):
     return str(leng)
         
 
-dst = input("enter destination:")
-dst = format_IP(dst)
+
 
 
 def show_messages_to_user(sender, message_from_client):
-    print('i am here')
     file_name = str(sender) + '.txt'
-    message_file = open(file_name, 'a')
-    message_file.write("message from peer ---------> " + message_from_client)
-    message_file.close()
+
+    try:
+        message_file = open(file_name, 'a')
+        try:
+            message_file.write("message from peer ---------> " + message_from_client)
+        except Exception as e:
+            print('this exception occured while writing to file: --->', e)
+        finally:
+            message_file.close()
+    except Exception as e:
+            print('this exception occured while opening file: --->', e)
 
 
 def receive_messages():
     while(True):
-        print('i am okay')
         #first header of packet
         sender = sock.recv(LEN_OF_IP).decode('utf-8')
         sender = sender.replace('*','')
@@ -86,15 +108,31 @@ def receive_messages():
 t_receive = threading.Thread(target=receive_messages)
 t_receive.start()
 
+
+
+while(True):
+    dst = input("enter destination:")
+    if(validate_ip_addresses(dst) == True):
+        dst = format_IP(dst)
+        break
+    else:
+        print('Your IP address format is wrong')
+
+
+
 while(True):
 
     message_to_client= input('enter message:') 
 
-    sock.sendall(dst.encode('utf-8')) #send first header (destination)
-    sock.sendall(format_len(message_to_client).encode('utf-8'))  #send second header (len of outgoing message)
-    sock.sendall(message_to_client.encode('utf-8')) #send third header (message itself)
+    #send first header (destination)
+    sock.sendall(dst.encode('utf-8')) 
 
-    #files = os.listdir()
+    #send second header (len of outgoing message)
+    sock.sendall(format_len(message_to_client).encode('utf-8'))  
+
+    #send third header (message itself)
+    sock.sendall(message_to_client.encode('utf-8')) 
+
 
     destination = str(dst).replace('*','') + '.txt'
     message_file = open(destination, 'a')
